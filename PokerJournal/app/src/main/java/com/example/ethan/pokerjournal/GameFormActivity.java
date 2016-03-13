f8e0ae4ed4167543ed68a45a026e061ad8d768b6
cyanidediamond <alee12131415@gmail.com>
2016-03-12 20:57:54 -0800

    added game form validators

--- PokerJournal/app/src/main/java/com/example/ethan/pokerjournal/GameFormActivity.java
+++ PokerJournal/app/src/main/java/com/example/ethan/pokerjournal/GameFormActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -24,19 +25,42 @@ public class GameFormActivity extends AppCompatActivity {
     public void onClickGameButton(View v) {
         DatabaseHelper db = new DatabaseHelper(this);
         Game game = new Game();
-        //get values
+        Toast toast = Toast.makeText(getApplication(), "Please fill all fields", Toast.LENGTH_SHORT);
+
+        //get values + validate
         Spinner spin = (Spinner) findViewById(R.id.spinnerGameType);
         String type = spin.getSelectedItem().toString();
         EditText editLocation = (EditText) findViewById(R.id.editLocaiton);
         String location = editLocation.getText().toString();
+        if(location.isEmpty()) {
+            toast.show();
+            return;
+        }
         EditText editDate = (EditText) findViewById(R.id.editDate);
         String date = editDate.getText().toString();
+        if (date.isEmpty()) {
+            toast.show();
+            return;
+        }
         EditText editTime = (EditText) findViewById(R.id.editTime);
+        if (editTime.getText().toString().isEmpty()) {
+            toast.show();
+            return;
+        }
         int time = Integer.parseInt(editTime.getText().toString());
         EditText editBuyIn = (EditText) findViewById(R.id.editBuyIn);
+        if (editBuyIn.getText().toString().isEmpty()) {
+            toast.show();
+            return;
+        }
         double buyIn = Double.parseDouble(editBuyIn.getText().toString());
         EditText editCashOut = (EditText) findViewById(R.id.editCashOut);
+        if (editCashOut.getText().toString().isEmpty()) {
+            toast.show();
+            return;
+        }
         double cashOut = Double.parseDouble(editCashOut.getText().toString());
+
         //set values
         game.setType(type);
         game.setLocation(location);
