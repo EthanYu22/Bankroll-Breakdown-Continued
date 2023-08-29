2113dfe8469c598011b933906b0ae7bc7016e8e9
Ethan Yu <EthanYu22@users.noreply.github.com>
2023-08-29 01:18:56 -0700

    Update LoginPage.java
    Simplify login page logic to something more easy to comprehend

--- PokerJournal/app/src/main/java/com/example/ethan/pokerjournal/LoginPage.java
+++ PokerJournal/app/src/main/java/com/example/ethan/pokerjournal/LoginPage.java
@@ -22,15 +22,15 @@ public class LoginPage extends AppCompatActivity
         prefs = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
         editor = prefs.edit();
 
-        if(!prefs.getBoolean("EnabledLoginPage", false)){
+        if(prefs.getBoolean("EnabledLoginPage", false) == true){
+            editor.putBoolean("EnabledLoginPage",true);
+            editor.commit();
+        } else {
             editor.putBoolean("EnabledLoginPage",false);
             editor.commit();
             Intent intent = new Intent(this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(intent);
-        } else {
-            editor.putBoolean("EnabledLoginPage",true);
-            editor.commit();
         }
     }
 
